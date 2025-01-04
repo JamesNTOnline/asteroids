ebbb9661c7e2988807d5a38a44b7f53e93868d35
JamesNTOnline <james.nicholas.thomas@gmail.com>
2025-01-04 04:24:36 +0000

    movement and game objects

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 # allows using code from open source pygame lib
 import pygame 
+import sys 
+
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+    print("Starting asteroids")
+    
+    # game object groups 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group() 
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable) # static field adding all Players to groups
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
-    while True:
+    # game objects
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    field = AsteroidField()
+
+    while True: # game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in asteroids:
+            if sprite.collides(player):
+                sys.exit("Game over!")
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 
--- set_display.sh
+++ set_display.sh
@@ -1,1 +1,2 @@
-export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk "{print $2}"):0
+export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+echo "script run"
